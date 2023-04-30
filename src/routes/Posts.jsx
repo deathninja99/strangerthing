7365646db4bc4567b38bf43fd6f9fc23682040ee
Charlotte Sass <Jeffreymsass@gmail.com>
2023-04-30 11:10:04 -0400

    forgot to add

--- src/routes/Posts.jsx
+++ src/routes/Posts.jsx
@@ -1,5 +1,6 @@
-import { FetchPosts } from "../api/fetchapi";
+import { FetchPosts, makeposting } from "../api/fetchapi";
 import { useEffect, useState } from "react";
+// gathers all posts and shows them
 export function Post() {
   //get posts and set them with usestate
   const [posts, setposts] = useState([]);
@@ -20,7 +21,73 @@ export function Post() {
             className="card"
             onClick={() => {
               {
-                console.log(posts.data);
+                posts.data;
+              }
+            }}
+          >
+            <div className="cardtext" key={post._id} id={post._id}>
+              {post.title}
+              <br></br>
+              {post.description}
+              <br />
+              {post.author.username}
+            </div>
+          </div>
+        </>
+      ))}
+    </div>
+  );
+}
+
+export function Postering() {
+  return (
+    <>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          const form = event.target;
+          const formData = new FormData(form);
+          const values = Object.fromEntries(formData.entries());
+          console.log(values);
+          makeposting();
+        }}
+      >
+        <input type="text" name="title" placeholder="title" />
+        <br />
+        <input type="text" name="price" placeholder="price" />
+        <br />
+        <p>will you deliver?</p>
+        <input type="checkbox" name="will_deliver" />
+        {"checked, for yes, unchecked for no"}
+        <br />
+        <input type="text" name="description" placeholder="description" />
+        <br />
+        <button>submit</button>
+      </form>
+    </>
+  );
+}
+
+export function singlepost(id) {
+  //get posts and set them with usestate
+  const [posts, setposts] = useState([]);
+  useEffect(() => {
+    async function getp() {
+      const watermellon = await FetchPosts();
+      setposts(watermellon);
+    }
+    getp();
+  }, []);
+  return (
+    //posts in card format
+    <div className="body">
+      <h1>Posts</h1>
+      {posts.map((post) => (
+        <>
+          <div
+            className="card"
+            onClick={() => {
+              {
                 posts.data;
               }
             }}
